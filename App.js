e579f899db4a2dc0ee0171373494990106b65b60
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-02-16 14:46:15 -0800

    extracted stack navigator as a component

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { createContext, useContext, useState } from "react";
-import Start from "./Screens/Start";
-import AddAnActivity from "./Screens/AddAnActivity";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Color } from "./Helpers/Color";
-import TabNavigator from "./Components/TabNavigator";
+import StackNavigator from "./Components/StackNavigator";
 
 export const EntriesContext = createContext(null);
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [entries, setEntries] = useState([]);
@@ -19,37 +15,7 @@ export default function App() {
       <StatusBar style="auto" />
       <EntriesContext.Provider value={{ entries, setEntries }}>
         <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              contentStyle: { backgroundColor: Color.background },
-            }}
-          >
-            <Stack.Screen
-              name="Start"
-              component={Start}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="AddAnActivity"
-              component={AddAnActivity}
-              options={{
-                headerBackTitleVisible: false,
-                title: "Add An Activity",
-                headerStyle: {
-                  backgroundColor: Color.general,
-                },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
-            />
-            <Stack.Screen
-              name="TabNavigator"
-              component={TabNavigator}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
+          <StackNavigator />
         </NavigationContainer>
       </EntriesContext.Provider>
     </View>
